1816e0da80385e340900d8a3134a68dce67dccc4
gera <german_abramov@epam.com>
2019-11-16 11:35:48 +0300

    initial commit

---
+++ src/main/kotlin/dev/optimist/dashboardx/DashboardxApplication.kt
@@ -0,0 +1,12 @@
+package dev.optimist.dashboardx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class DashboardxApplication
+
+fun main(args: Array<String>) {
+	SpringApplication.run(DashboardxApplication::class.java, *args)
+}
+
