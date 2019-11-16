1f5eb460206e6334f92c84321deba9365f357861
Sergey Medvedev <sergey.medvedev@onetick.com>
2019-11-16 17:09:42 +0200

    Add emotion entity

---
+++ src/main/kotlin/dev/optimist/dashboardx/api/entity/Emotion.kt
@@ -0,0 +1,36 @@
+package dev.optimist.dashboardx.api.entity
+
+data class Emotion (
+        val anger: Double,
+        val contempt: Double,
+        val disgust: Double,
+        val fear: Double,
+        val happiness: Double,
+        val neutral: Double,
+        val sadness: Double,
+        val surprise: Double
+) {
+    companion object {
+        fun fuckYura(): Emotion {
+            return Emotion(
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+            )
+        }
+    }
+}
+
+fun List<Emotion>.average(): Emotion {
+    if (this.isEmpty()) return Emotion.fuckYura()
+
+    val size = this.size
+    return Emotion(
+            this.sumByDouble { it.anger }/size,
+            this.sumByDouble { it.contempt }/size,
+            this.sumByDouble { it.disgust }/size,
+            this.sumByDouble { it.fear }/size,
+            this.sumByDouble { it.happiness }/size,
+            this.sumByDouble { it.neutral }/size,
+            this.sumByDouble { it.sadness }/size,
+            this.sumByDouble { it.surprise }/size
+    )
+}
